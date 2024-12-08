eed692c43000ae95e74d1a7379a8274e3834ced9
mosaab radi <mosp.rady@gmail.com>
2024-12-08 23:17:52 +0200

    change chat type to be a text

--- chatApp.api/MappingProfiles.cs
+++ chatApp.api/MappingProfiles.cs
@@ -11,6 +11,12 @@ public class MappingProfiles : Profile
   {
     // CreateMap<Project, ProjectDto>();
     CreateMap<AppUser, UserDto>();
+    CreateMap<ChatModel, ChatDto>()
+     .ForMember(c => c.Type, opt => opt.MapFrom(src => Enum.GetName(src.Type)));
+    CreateMap<Message, MessageDto>();
+    CreateMap<Participant, ParticipantDto>();
+    CreateMap<Notification, NotificationDto>();
+    // CreateMap<CreateChatDto, ChatModel>();
 
     // CreateMap<Issue, IssueDto>()
     // .ForMember(issueDto => issueDto.Status, opt => opt.MapFrom(src => Enum.GetName(typeof(Status), src.Status)))
