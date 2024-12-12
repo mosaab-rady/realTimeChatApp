b110c9ed5350fc72f617e1468bd429f6b0ec8b28
mosaab radi <mosp.rady@gmail.com>
2024-12-12 19:07:59 +0200

    add participant and notifications controller

---
+++ chatApp.api/Controllers/NotificationController.cs
@@ -0,0 +1,152 @@
+using AutoMapper;
+using chatApp.Dtos;
+using chatApp.Entities;
+using chatApp.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chatApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationController(
+  INotificationService notificationService,
+  UserManager<AppUser> userManager,
+  IMessageService messageService,
+  IMapper mapper) : ControllerBase
+{
+  // 1) get all notification
+  [HttpGet]
+  public async Task<IEnumerable<NotificationDto>> GetAllNotifications()
+  {
+    var notifications = await notificationService.GetAllNotificationsAsync();
+    return mapper.Map<IEnumerable<NotificationDto>>(notifications);
+  }
+
+  // 2) get notification by id
+  [HttpGet("{id}")]
+  public async Task<ActionResult<NotificationDto>> GetNotificationById(Guid id)
+  {
+    Notification notification = await notificationService.GetNotificationByIdAsync(id);
+
+    if (notification is null)
+    {
+      return Problem(
+        detail: $"No notification found with id '{id}'.",
+        statusCode: StatusCodes.Status404NotFound);
+    }
+
+    return mapper.Map<NotificationDto>(notification);
+  }
+  // 3) get user notifications by user id
+  [HttpGet("{userId}/notifications")]
+  public async Task<ActionResult<IEnumerable<NotificationDto>>> GetUserNotifications(string userId)
+  {
+    AppUser appUser = await userManager.FindByIdAsync(userId);
+
+    if (appUser is null)
+    {
+      return Problem(
+        detail: $"No user found with id '{userId}'.",
+        statusCode: StatusCodes.Status404NotFound
+      );
+    }
+
+    IEnumerable<Notification> notifications = await notificationService.GetUserNotificationsByUserIdAsync(userId);
+
+    return Ok(mapper.Map<IEnumerable<NotificationDto>>(notifications));
+  }
+
+
+  // 4) get my notifications
+
+  [HttpGet("mynotifications")]
+  public async Task<ActionResult<IEnumerable<NotificationDto>>> GetMyNotifications()
+  {
+    string userEmail = User.Identity.Name;
+
+    AppUser appUser = await userManager.FindByEmailAsync(userEmail);
+
+    if (appUser is null)
+    {
+      return Problem(
+        detail: $"No user found with email '{userEmail}'.",
+        statusCode: StatusCodes.Status404NotFound
+      );
+    }
+
+    IEnumerable<Notification> notifications = await notificationService.GetUserNotificationsByUserIdAsync(appUser.Id);
+
+    return Ok(mapper.Map<IEnumerable<NotificationDto>>(notifications));
+  }
+
+
+  // 5) get un read notification
+  [HttpGet("mynotifications/unread")]
+  public async Task<ActionResult<IEnumerable<NotificationDto>>> GetMyUnReadNotifications()
+  {
+    string userEmail = User.Identity.Name;
+
+    AppUser appUser = await userManager.FindByEmailAsync(userEmail);
+
+    if (appUser is null)
+    {
+      return Problem(
+        detail: $"No user found with email '{userEmail}'.",
+        statusCode: StatusCodes.Status404NotFound
+      );
+    }
+
+    IEnumerable<Notification> notifications =
+    await notificationService.GetMyUnReadNotificationsAsync(appUser.Id);
+
+    return Ok(mapper.Map<IEnumerable<NotificationDto>>(notifications));
+  }
+
+  // 6) create new notification
+  [HttpPost]
+  public async Task<IActionResult> CreateNotification(CreateNotificationDto createNotificationDto)
+  {
+    AppUser appUser = await userManager.FindByIdAsync(createNotificationDto.User_id);
+    Message message = await messageService.GetMessageByIdAsync(new Guid(createNotificationDto.Message_id));
+
+    if (appUser is null | message is null)
+    {
+      return Problem(
+        detail: "No user or message found.",
+        statusCode: StatusCodes.Status404NotFound
+        );
+    }
+
+    Notification notification = new()
+    {
+      Message_id = message.Id,
+      Message = message,
+      User = appUser,
+      User_id = appUser.Id
+    };
+
+    await notificationService.CreateNewNotificationAsync(notification);
+
+    return CreatedAtAction("CreateNotification", new { detail = "Created New Notification" });
+  }
+
+
+  // 7) delete notification
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteNotification(Guid id)
+  {
+    Notification notification = await notificationService.GetNotificationByIdAsync(id);
+
+    if (notification is null)
+    {
+      return Problem(
+        detail: $"No Notification found with id '{id}'.",
+        statusCode: StatusCodes.Status404NotFound);
+    }
+
+    await notificationService.DeleteNotificationByIdAsync(id);
+
+    return NoContent();
+  }
+}
---
+++ chatApp.api/Controllers/ParticipantController.cs
@@ -0,0 +1,105 @@
+using AutoMapper;
+using chatApp.Dtos;
+using chatApp.Entities;
+using chatApp.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chatApp.Api.Controllers;
+
+[ApiController]
+[Route("api/controller")]
+public class ParticipantController(
+  IParticipantService participantService,
+  IMapper mapper,
+  IChatService chatService,
+  UserManager<AppUser> userManager) : ControllerBase
+{
+  // 1) get all participants
+  [HttpGet]
+  public async Task<IEnumerable<ParticipantDto>> GetAllParticipants() =>
+   mapper.Map<IEnumerable<ParticipantDto>>(await participantService.GetAllParticipantsAsync());
+
+
+  // 2) get all participants in a chat
+  [HttpGet("{chatId}/participants")]
+  public async Task<ActionResult<IEnumerable<ParticipantDto>>> GetAllParticipantsInChat(Guid chatId)
+  {
+    ChatModel chatModel = await chatService.GetChatByIdAsync(chatId);
+
+    if (chatModel is null)
+    {
+      return Problem(
+        detail: $"No chat found with id '{chatId}'.",
+        statusCode: StatusCodes.Status404NotFound);
+    }
+
+    IEnumerable<Participant> participants = await participantService.GetParticipantsInChatAsync(chatId);
+
+    return Ok(mapper.Map<IEnumerable<ParticipantDto>>(participants));
+  }
+
+
+  // 3) get participant by id
+  [HttpGet("{id}")]
+  public async Task<ActionResult<ParticipantDto>> GetParticipant(Guid id)
+  {
+    Participant participant = await participantService.GetParticipantByIdAsync(id);
+
+    if (participant is null)
+    {
+      return Problem(
+        detail: $"No participant found with id '{id}'.",
+        statusCode: StatusCodes.Status404NotFound);
+    }
+
+    return mapper.Map<ParticipantDto>(participant);
+  }
+
+  // 4) crete new participant
+  [HttpPost]
+  public async Task<IActionResult> CreateNewParticipant(CreateParticipantDto createParticipantDto)
+  {
+    ChatModel chatModel = await chatService.GetChatByIdAsync(new Guid(createParticipantDto.Chat_id));
+    AppUser appUser = await userManager.FindByIdAsync(createParticipantDto.User_id);
+
+
+    if (appUser is null | chatModel is null)
+    {
+      return Problem(
+        detail: "No user or chat found.",
+        statusCode: StatusCodes.Status404NotFound
+        );
+    }
+
+    Participant participant = new()
+    {
+      Chat = chatModel,
+      Chat_id = chatModel.Id,
+      Role = createParticipantDto.Role,
+      User = appUser,
+      User_id = appUser.Id
+    };
+
+
+    await participantService.CreateNewParticipantAsync(participant);
+
+    return CreatedAtAction("CreateNewParticipant", new { detail = "Created Participant." });
+  }
+  // 5) delete participant
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteParticipant(Guid id)
+  {
+    Participant participant = await participantService.GetParticipantByIdAsync(id);
+
+    if (participant is null)
+    {
+      return Problem(
+        detail: $"No participant found with id '{id}'.",
+        statusCode: StatusCodes.Status404NotFound);
+    }
+
+    await participantService.DeleteParticipantByIdAsync(id);
+    return NoContent();
+  }
+}
