f8432d2800ba89a892f6aeb19d4831785fff00ed
mosaab radi <mosp.rady@gmail.com>
2024-12-24 22:03:59 +0200

    add page module

---
+++ chatApp.angular/src/app/page/page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageRoutingModule } from './page-routing.module';
+import { PageComponent } from './components/page/page.component';
+
+@NgModule({
+  declarations: [PageComponent],
+  imports: [CommonModule, PageRoutingModule],
+})
+export class PageModule {}
