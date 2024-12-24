b195f3c99411f2c09821cf329a0a09e397c2514a
mosaab radi <mosp.rady@gmail.com>
2024-12-24 22:01:54 +0200

    add routes for auth

--- chatApp.angular/src/app/app.routes.ts
+++ chatApp.angular/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/Components/login/login.component';
+import { SignupComponent } from './auth/Components/signup/signup.component';
+import { authGuard } from './auth/Services/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', title: 'Login | Chat App', component: LoginComponent },
+  { path: 'signup', title: 'Sign up | Chat App', component: SignupComponent },
+  {
+    path: '',
+    canActivate: [authGuard],
+    loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
+  },
+];
