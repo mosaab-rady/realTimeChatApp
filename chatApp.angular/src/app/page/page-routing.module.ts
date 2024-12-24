b9325893d36e62abf2f85a197d485484203eb587
mosaab radi <mosp.rady@gmail.com>
2024-12-24 22:04:22 +0200

    add page routing

---
+++ chatApp.angular/src/app/page/page-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './components/page/page.component';
+
+const routes: Routes = [
+  { path: '', title: 'Chat App', component: PageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PageRoutingModule {}
