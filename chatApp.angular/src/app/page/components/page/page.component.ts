87ad6820a534aff91927cfeef7b189f67906ec74
mosaab radi <mosp.rady@gmail.com>
2024-12-28 22:17:00 +0200

    add reference to users,chats components

--- chatApp.angular/src/app/page/components/page/page.component.ts
+++ chatApp.angular/src/app/page/components/page/page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AllUsersComponent } from '../all-users/all-users.component';
+import { PrivateChatsComponent } from '../private-chats/private-chats.component';
+import { GroupChatsComponent } from '../group-chats/group-chats.component';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrl: './page.component.css',
-  standalone: false,
+  imports: [AllUsersComponent, PrivateChatsComponent, GroupChatsComponent],
 })
 export class PageComponent {}
