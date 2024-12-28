3c456e18afc62b686e8b155c80a21f0ca6438272
mosaab radi <mosp.rady@gmail.com>
2024-12-28 22:16:20 +0200

    add group chats component

---
+++ chatApp.angular/src/app/page/components/group-chats/group-chats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-chats',
+  imports: [],
+  templateUrl: './group-chats.component.html',
+  styleUrl: './group-chats.component.css'
+})
+export class GroupChatsComponent {
+
+}
