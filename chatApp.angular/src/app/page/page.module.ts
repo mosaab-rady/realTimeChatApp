42415618da9e25c8cfafceb6834a05be8a74c098
mosaab radi <mosp.rady@gmail.com>
2024-12-28 22:15:44 +0200

    remove page component declaration

--- chatApp.angular/src/app/page/page.module.ts
+++ chatApp.angular/src/app/page/page.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PageRoutingModule } from './page-routing.module';
-import { PageComponent } from './components/page/page.component';
 
 @NgModule({
-  declarations: [PageComponent],
+  declarations: [],
   imports: [CommonModule, PageRoutingModule],
 })
 export class PageModule {}
