0b4fa31e202da06a3794af767491738341ba64a2
mosaab radi <mosp.rady@gmail.com>
2025-01-04 15:01:42 +0200

    add private chats

--- chatApp.angular/src/app/page/components/private-chats/private-chats.component.ts
+++ chatApp.angular/src/app/page/components/private-chats/private-chats.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../Services/chat.service';
+import { AuthService } from '../../../auth/Services/auth.service';
+import { ChatModel } from '../../../../Models/Chats/ChatModel';
+import { UserRawComponent } from '../user-raw/user-raw.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-private-chats',
-  imports: [],
+  imports: [UserRawComponent, CommonModule],
   templateUrl: './private-chats.component.html',
-  styleUrl: './private-chats.component.css'
+  styleUrl: './private-chats.component.css',
 })
-export class PrivateChatsComponent {
-
+export class PrivateChatsComponent implements OnInit {
+  chats: ChatModel[] = [];
+  /**
+   *
+   */
+  constructor(private chatService: ChatService, private auth: AuthService) {}
+  ngOnInit(): void {
+    this.chatService
+      .GetPrivateChats(this.auth.USER?.id!)
+      .subscribe((chats) => (this.chats = chats));
+  }
 }
