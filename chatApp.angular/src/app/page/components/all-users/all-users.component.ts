a7a91be75295523651ddf0d601f492a959bfc434
mosaab radi <mosp.rady@gmail.com>
2025-01-04 14:58:38 +0200

    remove user raw from here for now

--- chatApp.angular/src/app/page/components/all-users/all-users.component.ts
+++ chatApp.angular/src/app/page/components/all-users/all-users.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../../../../Models/Users/UserModel';
+import { enviroment } from '../../../../enviroment/enviroment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-all-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-users.component.html',
   styleUrl: './all-users.component.css',
 })
-export class AllUsersComponent {
-  // 1) get all users
+export class AllUsersComponent implements OnInit {
+  users: UserModel[] = [];
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    // 1) get all users
+    this.http
+      .get<UserModel[]>(`${enviroment.apiUrl}/api/users`, {
+        withCredentials: true,
+      })
+      .subscribe((e) => (this.users = e));
+  }
 }
