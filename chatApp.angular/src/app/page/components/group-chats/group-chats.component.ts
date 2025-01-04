8c6ae5d4e3e364c98970d5739638a0b78f5da555
mosaab radi <mosp.rady@gmail.com>
2025-01-04 15:00:11 +0200

    remove user raw for now

--- chatApp.angular/src/app/page/components/group-chats/group-chats.component.ts
+++ chatApp.angular/src/app/page/components/group-chats/group-chats.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatModel } from '../../../../Models/Chats/ChatModel';
+import { ChatService } from '../../Services/chat.service';
+import { AuthService } from '../../../auth/Services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-group-chats',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './group-chats.component.html',
-  styleUrl: './group-chats.component.css'
+  styleUrl: './group-chats.component.css',
 })
-export class GroupChatsComponent {
-
+export class GroupChatsComponent implements OnInit {
+  chats: ChatModel[] = [];
+  /**
+   *
+   */
+  constructor(private chatService: ChatService, private auth: AuthService) {}
+  ngOnInit(): void {
+    this.chatService
+      .GetGroupChats(this.auth.USER?.id!)
+      .subscribe((chats) => (this.chats = chats));
+  }
 }
