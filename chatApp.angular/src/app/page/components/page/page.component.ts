750828310af830bbaa43f885e3b6d459727a759e
mosaab radi <mosp.rady@gmail.com>
2025-01-04 15:01:11 +0200

    add chat box container

--- chatApp.angular/src/app/page/components/page/page.component.ts
+++ chatApp.angular/src/app/page/components/page/page.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { AllUsersComponent } from '../all-users/all-users.component';
 import { PrivateChatsComponent } from '../private-chats/private-chats.component';
 import { GroupChatsComponent } from '../group-chats/group-chats.component';
+import { ChatBoxContainerComponent } from '../chat-box-container/chat-box-container.component';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrl: './page.component.css',
-  imports: [AllUsersComponent, PrivateChatsComponent, GroupChatsComponent],
+  imports: [
+    PrivateChatsComponent,
+    GroupChatsComponent,
+    ChatBoxContainerComponent,
+  ],
 })
 export class PageComponent {}
